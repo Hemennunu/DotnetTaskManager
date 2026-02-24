621e2c434d963b7a74701b82f91c04f350dccc20
Hemennunu <nuhaminarega19@gmail.com>
2026-02-24 10:41:22 +0300

    deleted some unwanted files

--- TaskFlow.Infrastructure/Services/AuthService.cs
+++ TaskFlow.Infrastructure/Services/AuthService.cs
@@ -58,7 +58,7 @@ namespace TaskFlow.Infrastructure.Services
             var tokenDescriptor = new SecurityTokenDescriptor
             {
                 Subject = new ClaimsIdentity(claims),
-                Expires = DateTime.UtcNow.AddHours(24),
+                Expires = DateTime.UtcNow.AddMinutes(15),
                 Issuer = _configuration["Jwt:Issuer"],
                 Audience = _configuration["Jwt:Audience"],
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
